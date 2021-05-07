6e708caed12334ddd4ff51052b7adb40e34c7cf3
noeljns <s0558239@htw-berlin.de>
2021-05-07 21:20:11 +0200

    Add result class and try to set up vaadin component

--- exercise2/src/custom-elements/results-table.js
+++ exercise2/src/custom-elements/results-table.js
@@ -5,11 +5,19 @@ template.innerHTML = `
     <style>
 
     </style>
+    
     <div class="wrapper">
         Results Table
-        <div id="table"></div>
-        <vaadin-grid></vaadin-grid>
+        <vaadin-grid id="table">
+            <vaadin-grid-column path="item.time" header="Zeit in ms"></vaadin-grid-column>
+        </vaadin-grid>
     </div>
+
+    <script>
+        customElements.whenDefined('vaadin-grid').then(function) {
+            document.querySelector('vaadin-grid').items = this.results
+        });
+    </script>
     `;
 
 class ResultsTable extends HTMLElement {
@@ -21,8 +29,22 @@ class ResultsTable extends HTMLElement {
         this.table = this.shadowRoot.querySelector("#table");
     }
 
-    setData(results) {
-        this.data = results;
+    setData(measurements) {
+        this.data = measurements;
+
+        this.results = this.getResultObjects();
+        console.log("hm")
+        console.log(this.results)
+    }
+
+    getResultObjects() {
+        var array = []
+
+        for (const ele of this.data) {
+            const result = new Result(ele.options, ele.time, ele.distractor_rank, ele.found);
+            array.push(result);    
+        }
+        return array;
     }
 
     showTable() {
@@ -32,3 +54,13 @@ class ResultsTable extends HTMLElement {
 }
 
 customElements.define("results-table", ResultsTable);
+
+
+class Result {
+    constructor(options, time, distractor_rank, found) {
+        this.options = options;
+        this.time = time;
+        this.distractor_rank = distractor_rank
+        this.found = found;
+    }
+}
