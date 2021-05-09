0413505dfd1e6d467947b984626e98049c0b69e5
Paula <s0558576@htw-berlin.de>
2021-05-09 09:04:38 +0200

    Show some data in the table

--- exercise2/src/custom-elements/results-table.js
+++ exercise2/src/custom-elements/results-table.js
@@ -3,21 +3,17 @@ const template = document.createElement("template");
 
 template.innerHTML = `
     <style>
-
     </style>
-    
+
     <div class="wrapper">
         Results Table
         <vaadin-grid id="table">
-            <vaadin-grid-column path="item.time" header="Zeit in ms"></vaadin-grid-column>
+            <vaadin-grid-column></vaadin-grid-column>
+            <vaadin-grid-column></vaadin-grid-column>
+            <vaadin-grid-column></vaadin-grid-column>
+            <vaadin-grid-column></vaadin-grid-column>
         </vaadin-grid>
     </div>
-
-    <script>
-        customElements.whenDefined('vaadin-grid').then(function) {
-            document.querySelector('vaadin-grid').items = this.results
-        });
-    </script>
     `;
 
 class ResultsTable extends HTMLElement {
@@ -30,37 +26,45 @@ class ResultsTable extends HTMLElement {
     }
 
     setData(measurements) {
-        this.data = measurements;
+        this.results = this.getResultObjects(measurements);
+        //TODO: before setting the data for the table it has to be grouped and brought in the right format for the table since we decided to display the data differently.
+        this.table.items = this.results;
+        const columns = this.table.querySelectorAll("vaadin-grid-column");
 
-        this.results = this.getResultObjects();
-        console.log("hm")
-        console.log(this.results)
+        columns[0].headerRenderer = function (root) {
+            root.textContent = "1000ms";
+        };
+        columns[0].renderer = function (root, column, model) {
+            root.textContent = model.item.found;
+        };
+
+        columns[1].headerRenderer = function (root) {
+            root.textContent = "500ms";
+        };
+        columns[1].renderer = function (root, column, model) {
+            root.textContent = model.item.found;
+        };
+        console.log(this.results);
     }
 
-    getResultObjects() {
-        var array = []
+    getResultObjects(data) {
+        var array = [];
 
-        for (const ele of this.data) {
+        for (const ele of data) {
             const result = new Result(ele.options, ele.time, ele.distractor_rank, ele.found);
-            array.push(result);    
+            array.push(result);
         }
         return array;
-    }
-
-    showTable() {
-        this.table.innerHTML = "this is the table";
-        this.table.innerHTML += " " + this.data[0].options[0];
     }
 }
 
 customElements.define("results-table", ResultsTable);
 
-
 class Result {
     constructor(options, time, distractor_rank, found) {
         this.options = options;
         this.time = time;
-        this.distractor_rank = distractor_rank
+        this.distractor_rank = distractor_rank;
         this.found = found;
     }
 }
